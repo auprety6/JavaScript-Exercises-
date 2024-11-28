35fc1ef19477c91cae24c4607ecefe2c93ddaff0
Anu Uprety <anu6@vt.edu>
2024-11-28 01:14:23 -0500

    Add files via upload

---
+++ Test.JS
@@ -0,0 +1,14 @@
+// Declare and initialize a sample 2-D array
+var a = [[1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 26, 7]];
+
+// Iterate through each row in the 2-D array
+for (var i in a) {
+   // Output the current row index
+   console.log("row " + i);
+   
+   // Iterate through each element in the current row
+   for (var j in a[i]) {
+      // Output the current element value
+      console.log(" " + a[i][j]);
+   }
+}
